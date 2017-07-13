3f9b725f6c849426908619e3dd756a18ff2dffa6
Peter Jang <peter.x.jang@gmail.com>
2017-07-12 19:21:26 -0500

    Use model method to display ingredients as a list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,18 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    array = ingredients.split(", ")
+    new_array = []
+    array.each do |ingredient|
+      if ingredient.ends_with?(".")
+        new_array << ingredient.upcase.chomp(".")
+      else
+        new_array << ingredient.upcase
+      end
+    end
+    return new_array
+  end
+
+  def directions_list
+    return directions.split(", ")
+  end
 end
