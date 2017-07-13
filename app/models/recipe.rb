d49688060489c86a732bccb27565d50a7e6e5dd9
Peter Jang <peter.x.jang@gmail.com>
2017-07-12 19:35:10 -0500

    Use model methods to display friendly created at and prep times

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,4 +15,17 @@ class Recipe < ApplicationRecord
   def directions_list
     return directions.split(", ").map { |direction| direction.capitalize }
   end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
